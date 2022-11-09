b15ce579af83c64f1e01cfa5a6bfa74040bca284
Sanjay Kumar Jat <sanjay1999.jat@gmail.com>
2022-11-09 16:43:13 +0530

    i have done

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let n=5;let count=0;
-for(let i=1;i<n;i++){
-if(i%2==0){
+for(let i=1;i<=n;i++){
+if(n[i]%2==0){
 
 count++;
 }
 }
-if(count>2){
+if(count!=2){
 console.log("not a prime");
 }
 else{
